3e39754b0e7e0d3aa5810fdb6485233e98073d18
patifstanga <patricia.2022319911@aluno.iffar.edu.br>
2024-08-01 13:39:21 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Nexum</title>
+</head>
+<body>
+    <div>
+        <form action="login.php" method="POST">
+            <div>
+                <label>CPF</label>
+                <input type="text" name="cpf">
+            </div>
+            <div>
+                <label>Senha</label>
+                <input type="password" name="senha">
+            </div>
+            <div>
+                <button type="submit">Enviar</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
