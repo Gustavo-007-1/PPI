09d376a79aa69b4798ffb68d9865d60e111e187a
Gustavo-007-1 <gustavo.2022303377@aluno.iffar.edu.br>
2024-09-05 10:36:08 -0300

    Delete PPI-main directory

--- PPI-main/nexum/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//configura��es para acesso ao BD
-define('BD_USER', 'root'); // USE O TEU USU�RIO DE BANCO DE DADOS
-define('BD_PASS', ''); // USE A TUA SENHA DO BANCO DE DADOS
-define('BD_NAME', 'nexum'); // USE O NOME DO TEU BANCO DE DADOS
-
-$conexao = mysqli_connect('localhost', BD_USER, BD_PASS);
-mysqli_select_db($conexao, BD_NAME);
-?>
